3f99e2a2891ffdcd5db7a4fac8430bb6dc25f3b1
Aga <aga@git.pl>
2020-02-23 11:05:28 +0100

    Added finding min and max

--- src/FindMaxMin.java
+++ src/FindMaxMin.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class FindMaxMin {
     public static void main(String[] args) {
-        
+
+        int[] tab = {1, 12, 15, 17, 20, 6, 5};
+        int max = findMaximu(tab);
+        int min = findMinimum(tab);
+
+        System.out.println(" Max: " + max + " Min: " + min);
+
     }
+
+    private static int findMinimum(int[] tab) {
+        int potentialMin = tab[0];
+        for (int i = 1; i < tab.length; i++) {
+            if (tab[i] < potentialMin)
+                potentialMin = tab[i];
+        }
+        return potentialMin;
+    }
+
+    private static int findMaximu(int[] tab) {
+        int potentialMax = tab[0];
+
+        for (int i = 1; i < tab.length; i++) {
+            if (tab[i] > potentialMax)
+                potentialMax = tab[i];
+
+        }
+        return potentialMax;
+    }
+
+
+
+
+
+
 }
+
+
+
+
+
